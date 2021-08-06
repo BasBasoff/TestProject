a73aa2a97831a625ec42dec02a4a54feb1cb4876
Даниил Ощепков <oshepkow.daniil@yandex.ru>
2021-08-06 16:19:15 +0700

    delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-HOST = 'localhost'    # The remote host
-PORT = 50007              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'0002 C1 01:13:02.877 00CR')
-    data = s.recv(1024).decode('utf-8')
-print('Received', repr(data))
